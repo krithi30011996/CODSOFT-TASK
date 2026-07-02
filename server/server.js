450296087a4ca5187acfbac66050a2d4e59b878f
krithi30011996 <kavipriyamagalingam@gmail.com>
2026-07-02 12:34:51 +0530

    dns changes

--- server/server.js
+++ server/server.js
@@ -1,25 +1,20 @@
-import dns from 'dns';
-dns.setDefaultResultOrder('ipv4first');
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import dns from 'dns';
 import connectDB from './config/db.js';
 
-//intialize express
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
 const app = express();
 
-//connect to database
 await connectDB();
 
-
-//middlewars
 app.use(cors());
 app.use(express.json());
 
-//roues
 app.get('/', (req,res) => res.send("API Working"));
 
-//port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,() => {
     console.log(`Server is running on port ${PORT}`);
