2fe556baaa98943ad84a24aad42a4e2d7de0d0c2
krithi30011996 <kavipriyamagalingam@gmail.com>
2026-07-03 13:01:37 +0530

    update in controller1

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -5,7 +5,9 @@ const connectCloudinary = async() => {
         cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
         api_key: process.env.CLOUDINARY_API_KEY,
         api_secret: process.env.CLOUDINARY_API_SECRET
+       
     })
+     console.log('CLOUD NAME:', process.env.CLOUDINARY_CLOUD_NAME);
 }
 
 export default connectCloudinary;
