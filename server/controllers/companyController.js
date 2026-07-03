a22462262cf92a13118210ae9530e26f5ada3df9
krithi30011996 <kavipriyamagalingam@gmail.com>
2026-07-03 17:20:22 +0530

    update in job.js

--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -46,20 +46,43 @@ export const registerCompany = async(req, res) =>{
     }
     catch(err){
         res.json({success:false, message:err.message});
-        console.log(err);
+
 
     }
 
 
 }
 
-//new register
-
 
 
 
 // company login
 export const loginCompany = async(req, res) =>{
+    const {email, password} = req.body;
+    try{
+        const company = await Company.findOne({email});
+
+        if(bcrypt.compare(password, company.password)){
+            res.json({
+                success: true,
+                company:{
+                     _id: company._id,
+                name: company.name,
+                email: company.email,
+                image: company.image
+
+                },
+                token: generateToken(company._id)
+            })
+        }
+        else{
+            res.json({success:false, message:"Invalid email or password"})
+        }
+    }
+    catch(error){
+        res.json({success:false, message: error.message});
+
+    }
 
 }
 
